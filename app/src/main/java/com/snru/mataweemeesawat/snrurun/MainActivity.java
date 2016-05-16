4ba8972b7cf33e63b07619b4acc95c29e2598346
pammy <pammysara@gmail.com>
2016-05-15 23:33:59 -0700

    clickSignUpMain

--- app/src/main/java/com/snru/mataweemeesawat/snrurun/MainActivity.java
+++ app/src/main/java/com/snru/mataweemeesawat/snrurun/MainActivity.java
@@ -1,13 +1,22 @@
 package com.snru.mataweemeesawat.snrurun;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }   //Main Method
+
+    public void clickSignUpMain(View view) {
+        startActivity(new Intent(MainActivity.this, SignUp.class));
+
     }
-}
+
+}   //Main Class
